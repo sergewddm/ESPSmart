9aca8fddabd570729b6957e3c49edcd719e73ad2
Serge <serge_wddm@hotmail.com>
2019-07-01 11:40:19 +0300

    atributes added

---
+++ src/ESPSmart.cpp
@@ -0,0 +1,16 @@
+// ESPSmart is library for ESP8266 and ESP32 Arduino boards.
+// This library is an extension for MQTT automation.
+//
+// Copyright (c) 2019 ALab.
+//
+
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
---
+++ src/ESPSmart.h
@@ -0,0 +1,5 @@
+// ESPSmart is library for ESP8266 and ESP32 Arduino boards.
+// This library is an extension for MQTT automation.
+//
+// Copyright (c) 2019 ALab.
+//
