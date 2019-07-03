5ad9c248af9b86674c170ce58854ad765d19c549
sergewddm <serge_wddm@tut.by>
2019-07-03 23:19:26 +0300

    update

--- src/ESPSmart.h
+++ src/ESPSmart.h
@@ -1,5 +1,5 @@
 // ESPSmart is library for ESP8266 and ESP32 Arduino boards.
-// This library is an extension for MQTT automation.
+// This library is an extension for simple  MQTT automation.
 //
 // Copyright (c) 2019 ALab.
 //
